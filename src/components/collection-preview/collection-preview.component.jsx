69a30f20ca3a0c14916cecc8e5c39aca3c442a41
Dylan Martin <dylanmartin@Rachaels-MacBook-Pro.local>
2022-01-29 11:53:47 -0600

    Adding Shop page with child collection-preview component which has child  collection-item component

---
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import './collection-preview.styles.scss';
+import CollectionItem from "../collection-item/collection-item.component";
+
+const CollectionPreview = ({ title, items }) => (
+  <div className="collection-preview">
+    <h2 className="title">{title}</h2>
+    <div className="preview">
+      {items
+        .filter((item, i) => i < 4)
+        .map(({ id, ...otherItemProps }) => (
+          <CollectionItem key={id} {...otherItemProps} />
+        )
+      )}
+    </div>
+  </div>
+)
+
+export default CollectionPreview;
