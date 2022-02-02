522df778eea585edbd37876e48d92999ae8a55f6
Dylan Martin <dylanmartin@Rachaels-MacBook-Pro.local>
2022-02-02 14:44:57 -0600

    Added collections-overview component to control /shop routes views

--- src/index.js
+++ src/index.js
@@ -2,17 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
+
+import { store, persistor } from './redux/store';
+import reportWebVitals from './reportWebVitals';
 
 import './index.css';
-import store from './redux/store';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,29 +1,12 @@
 import React from 'react';
 
-import SHOP_DATA from './shop.data.js';
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      collections: SHOP_DATA
-    }
-  }
-
-  render() {
-    const { collections } = this.state;
-
-    return(
-      <div className="shop-page">
-        <h1>Collections</h1>
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    )
-  }
-}
+const ShopPage = () => (
+  <div className="shop-page">
+    <h1>Collections</h1>
+    <CollectionsOverview />
+  </div>
+)
 
 export default ShopPage;
