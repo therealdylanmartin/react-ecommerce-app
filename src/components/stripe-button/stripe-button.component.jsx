1e4ebceb84792407ebfe44e307f20edaf4b2a0af
Dylan Martin <dylanmartin@Rachaels-MacBook-Pro.local>
2022-02-02 18:02:26 -0600

    Adding stripe payment processing and new favicon and title to index.html

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_51KOsCiDg95MrvrmOUz3iOtqzYbfjGmge4C3npgRyU12gxa5tV7Dc5lfd4YgZ5QjxPMPUXEEgPrSYWVW2GI5nsDIi00FCfg5ymH';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Successful');
+  }
+
+  return(
+    <StripeCheckout 
+      label="Pay Now" 
+      name="CRWN Clothing Ltd." 
+      billingAddress 
+      shippingAddress 
+      image={null} 
+      description={`Your total is $${price}`} 
+      amount={priceForStripe} 
+      panelLabel="Pay Now" 
+      token={onToken} 
+      stripeKey={publishableKey} 
+    />
+  )
+}
+
+export default StripeCheckoutButton;
