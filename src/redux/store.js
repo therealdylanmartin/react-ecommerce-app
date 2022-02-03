ef4cd983a2840f527082f4c7e56bebcdcdcccd44
Dylan Martin <dylanmartin@Rachaels-MacBook-Pro.local>
2022-02-03 11:05:31 -0600

    Updated stripe checkout with store logo, turned off logger middleware when in production

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,11 @@ import logger from 'redux-logger';
 
 import rootReducer from './root.reducer';
 
-const middlewares = [logger];
+const middlewares = [];
+
+if (process.env.NODE_ENV === 'development') middlewares.push(logger);
+
+console.log(middlewares);
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
